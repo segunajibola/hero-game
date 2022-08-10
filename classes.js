fb7afcf66004893080c25a6de6c747dcd16b0832
Segun Ajibola <ajbl.segun@gmail.com>
2022-08-10 13:36:40 +0100

    firstCommit

--- classes.js
+++ classes.js
@@ -22,10 +22,14 @@ class Module {
         // copy data object to this. this is a copy of the
         // object coming in as a data parameter
         this.percentCompletedModule = this.studentsCompleted / this.studentsEnrolled * 100
+        // percentCompletedModule creates a new properties that
+        // do some maths with the copied object initial properties
+        // i.e data property
     }
     logPercentCompletedModule(){
         console.log(this.percentCompletedModule)
     }
+    // logPercentCompletedModule is a new method
 }
 
 const responsiveDesign = new Module(moduleStats.module3)
